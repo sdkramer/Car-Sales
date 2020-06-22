b3c3b18bc4dc892c21545235bbac11e3e90e559e
Sean Kramer <seandominickramer@gmail.com>
2020-06-22 15:18:39 -0400

    Add Reducers file; connect Header component to store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Header = props => {
+  console.log('header props', props);
+  
   return (
     <>
       <figure className="image is-128x128">
@@ -12,4 +15,10 @@ const Header = props => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+console.log('header state', state);
+
+  return { car: state.cars.car };
+}
+
+export default connect(mapStateToProps)(Header);
